d21123a9d461eb1802af95cc2fc65edfb83423dc
shaack <shaack@gmail.com>
2023-11-16 19:47:56 +0100

    command-i and command-b

--- src/ExtremeMinimalMdEditor.js
+++ src/ExtremeMinimalMdEditor.js
@@ -25,7 +25,10 @@ export class ExtremeMinimalMdEditor {
 
     handleKeyDown(e) {
         const start = this.element.selectionStart
+        const end = this.element.selectionEnd
         const before = this.element.value.substring(0, start)
+        const selected = this.element.value.substring(start, end)
+        const after = this.element.value.substring(end)
         const currentLine = before.substring(before.lastIndexOf('\n') + 1)
         const isListMode = currentLine.match(/^\t*- /)
         if (e.key === 'Tab') {
@@ -43,12 +46,20 @@ export class ExtremeMinimalMdEditor {
             const didHandleEnter = this.handleEnterKey()
             if (didHandleEnter) {
                 if (currentLine.match(/^\s*- $/)) {
-                    // end list mode, remove the last two lines
+                    // end list mode, remove the last line
                     this.element.value = before.substring(0, before.lastIndexOf('\n')) + "\n"
                 } else {
                     e.preventDefault()
                 }
             }
+        } else if ((e.ctrlKey || e.metaKey) && e.key === 'b') {
+            e.preventDefault()
+            this.element.value = before + '**' + selected + '**' + after
+            this.element.selectionStart = this.element.selectionEnd = start + 2 + selected.length + 2
+        } else if ((e.ctrlKey || e.metaKey) && e.key === 'i') {
+            e.preventDefault()
+            this.element.value = before + '_' + selected + '_' + after
+            this.element.selectionStart = this.element.selectionEnd = start + 1 + selected.length + 1
         }
     }
 
