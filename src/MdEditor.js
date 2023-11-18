73d99d0d70e20e9e0881d2be8f79680e9fd0d716
shaack <shaack@gmail.com>
2023-11-18 20:15:21 +0100

    Used (deprecated) `document.execCommand("insertText"…` and it works like a charm

--- src/MdEditor.js
+++ src/MdEditor.js
@@ -6,31 +6,13 @@ export class MdEditor {
 
     constructor(element) {
         this.element = element
-        this.undoRedoManager = new UndoRedoManager()
 
-        this.saveState()
         // listen to typing
         this.element.addEventListener('keydown', (e) => this.handleKeyDown(e))
-        this.element.addEventListener('input', () => {
-            this.saveState()
-        })
     }
 
-    saveState() {
-        const currentState = this.element.value
-        const execute = (state) => this.element.value = state
-        const unexecute = (state) => this.element.value = state
-        const command = new Command(execute, unexecute, currentState)
-        this.undoRedoManager.execute(command)
-    }
-
-    setValue(value) {
-        this.element.value = value
-        let event = new Event('input', {
-            bubbles: true,
-            cancelable: true,
-        })
-        this.element.dispatchEvent(event)
+    insertTextAtCursor(text) {
+        document.execCommand("insertText", false, text)
     }
 
     handleKeyDown(e) {
@@ -38,7 +20,6 @@ export class MdEditor {
         const end = this.element.selectionEnd
         const before = this.element.value.substring(0, start)
         const selected = this.element.value.substring(start, end)
-        const after = this.element.value.substring(end)
         const currentLine = before.substring(before.lastIndexOf('\n') + 1)
         const isListMode = currentLine.match(/^\t*- /)
         if (e.key === 'Tab') {
@@ -53,118 +34,73 @@ export class MdEditor {
                 this.insertTabAtCursorPosition()
             }
         } else if (e.key === 'Enter') {
-            const didHandleEnter = this.handleEnterKey()
-            if (didHandleEnter) {
-                if (currentLine.match(/^\s*- $/)) {
-                    // end list mode, remove the last line
-                    this.setValue(before.substring(0, before.lastIndexOf('\n')) + "\n")
-                } else {
-                    e.preventDefault()
-                }
-            }
+            this.handleEnterKey(e)
         } else if (e.ctrlKey || e.metaKey) {
             if (e.key === 'b') { // bold
                 e.preventDefault()
-                this.setValue(before + '**' + selected + '**' + after)
-                this.element.selectionStart = this.element.selectionEnd = start + 2 + selected.length + 2
+                if (selected) {
+                    this.insertTextAtCursor('**' + selected + '**')
+                } else {
+                    this.insertTextAtCursor('**')
+                }
             } else if (e.key === 'i') { // italic
                 e.preventDefault()
-                this.setValue(before + '_' + selected + '_' + after)
-                this.element.selectionStart = this.element.selectionEnd = start + 1 + selected.length + 1
-            } else if (e.key === 'g') { // game todo this could be an extension
-                e.preventDefault()
-                this.setValue(before + '[game id="' + selected + '"]' + after)
-                this.element.selectionEnd = this.element.selectionStart = start + 10
-            } else if (e.key === 'z') { // undo, redo
-                e.preventDefault()
-                if (e.shiftKey) {
-                    this.undoRedoManager.redo(this.element.value)
+                if (selected) {
+                    this.insertTextAtCursor('_' + selected + '_')
                 } else {
-                    this.undoRedoManager.undo(this.element.value)
+                    this.insertTextAtCursor('_')
                 }
+            } else if (e.key === 'e') { // game todo this could be an extension
+                e.preventDefault()
+                this.insertTextAtCursor('[game id="' + selected + '"]')
+                this.element.selectionStart = start + 10
+                this.element.selectionEnd = start + 10 + selected.length
             }
         }
     }
 
-    handleEnterKey() {
+    handleEnterKey(e) {
         const start = this.element.selectionStart
         const before = this.element.value.substring(0, start)
         const currentLine = before.substring(before.lastIndexOf('\n') + 1)
+        const matchEmpty = currentLine.match(/^(\s*- )$/)
         const match = currentLine.match(/^(\s*- )/)
-        if (match) {
-            const spaces = match[1]
-            const after = this.element.value.substring(start)
-            this.setValue(before + '\n' + spaces + after)
-            this.element.selectionStart = this.element.selectionEnd = start + spaces.length + 1
-            return true
+        if(matchEmpty) {
+            e.preventDefault()
+            const pre = matchEmpty[1]
+            this.element.selectionStart = before.lastIndexOf('\n') + 1
+            this.element.selectionEnd = this.element.selectionStart + pre.length
+            this.insertTextAtCursor('')
+        } else if (match) {
+            e.preventDefault()
+            const pre = match[1]
+            this.insertTextAtCursor('\n' + pre)
         }
-        return false
     }
 
     insertTabAtCursorPosition() {
-        const start = this.element.selectionStart
-        const end = this.element.selectionEnd
-        const before = this.element.value.substring(0, start)
-        const after = this.element.value.substring(end)
-        this.setValue(before + '\t' + after)
-        this.element.selectionStart = this.element.selectionEnd = start + 1
+        this.insertTextAtCursor('\t')
     }
 
     insertTabAtLineStart() {
         const start = this.element.selectionStart
         const before = this.element.value.substring(0, start)
         const lineStart = before.lastIndexOf('\n') + 1
-        const after = this.element.value.substring(start)
-        this.setValue(before.substring(0, lineStart) + '\t' + before.substring(lineStart) + after)
+        this.element.selectionStart = this.element.selectionEnd = lineStart
+        this.insertTextAtCursor('\t')
         this.element.selectionStart = this.element.selectionEnd = start + 1
     }
 
     removeTab() {
         const start = this.element.selectionStart
-        const end = this.element.selectionEnd
         const before = this.element.value.substring(0, start)
-        const after = this.element.value.substring(end)
         const lineStart = before.lastIndexOf('\n') + 1
         const currentLine = before.substring(lineStart)
         if (currentLine.startsWith('\t')) {
-            this.setValue(before.substring(0, lineStart) + currentLine.substring(1) + after)
+            this.element.selectionStart = lineStart
+            this.element.selectionEnd = lineStart + 1
+            this.insertTextAtCursor("")
             this.element.selectionStart = this.element.selectionEnd = start - 1
         }
-    }
-}
-
-class Command {
-    constructor(execute, unexecute, state) {
-        this.execute = execute
-        this.unexecute = unexecute
-        this.state = state
-    }
-}
-
-class UndoRedoManager {
-    constructor() {
-        this.undoStack = []
-        this.redoStack = []
-    }
-
-    execute(command) {
-        console.log("execute", command.state)
-        command.execute(command.state)
-        this.undoStack.push(command)
-        this.redoStack = [] // clear redoStack whenever new command is executed
-    }
-
-    undo() {
-        if (this.undoStack.length === 0) return
-        const command = this.undoStack.pop()
-        command.unexecute(command.state)
-        this.redoStack.push(command)
-    }
-
-    redo() {
-        if (this.redoStack.length === 0) return
-        const command = this.redoStack.pop()
-        command.execute(command.state)
-        this.undoStack.push(command)
     }
 }
